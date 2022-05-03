a16edde4d10b8efed75a38fa0ddbfff5712103ac
Drew M <drewmalette@protonmail.com>
2022-05-02 20:52:57 -0500

    Cleaned up DialogueBox prototype

--- example4.py
+++ example4.py
@@ -2,16 +2,17 @@ import pygame
 
 class DialogueBox:
     def __init__(self, font):
+        self.font = font
         self.lines = []
-        self.lines.append(DialogueLine("This is a line of text", font))
-        self.lines.append(DialogueLine("SaGa is an excellent series", font))
-        self.lines.append(DialogueLine("I Love Jia Ren", font))
+        self.lines.append(DialogueLine("Prototype dialogue box.", self))
+        self.lines.append(DialogueLine("Gets the job done for now.", self))
+        self.lines.append(DialogueLine("More will be added eventually.", self))
         
-        self.current = 0
+        self.current = 0 # current line
+        self.visible = True
         
         # flags
         self.COMPLETE = False
-        self.visible = True
         
     def update(self, tick):        
         for event in pygame.event.get():
@@ -37,13 +38,13 @@ class DialogueBox:
                 surface.blit(line.label, (10, 10 + 24 * i))
 
 class DialogueLine:
-    def __init__(self, text, font, parent=None): # parent will be DialogueBox
+    def __init__(self, text, parent): # parent will be DialogueBox
         self.text = text
         self.text_length = len(self.text)+1
-        self.font = font
+        self.parent = parent
         
         self.cursor = 0
-        self.label = self.font.render("", 0, (0xff,0xff,0xff))
+        self.label = self.parent.font.render("", 0, (0xff,0xff,0xff))
         
         # flags
         self.ENDED = False
@@ -55,11 +56,11 @@ class DialogueLine:
             self.ENDED = True
         if tick % 2 == 0 and self.cursor <= self.text_length and not self.ENDED:
             self.cursor += 1
-        self.label = self.font.render(self.text[:self.cursor], 0, (0xff,0xff,0xff))
+        self.label = self.parent.font.render(self.text[:self.cursor], 0, (0xff,0xff,0xff))
             
     def render(self, surface):
         if self.cursor < self.text_length:
-            self.label = self.font.render(self.text[:self.cursor], 0, (0xff,0xff,0xff))
+            self.label = self.parent.font.render(self.text[:self.cursor], 0, (0xff,0xff,0xff))
             surface.blit(self.label, (10,10))
 
 pygame.init()
@@ -71,27 +72,15 @@ font = pygame.font.Font(None, 24)
 
 dbox = DialogueBox(font)
 
-#line = "Hello, gamer. Welcome to pyRPG!"
-#line_length = len(line)+1
-#cursor = 0
-
 tick = 0
 
 while 1:
     clock.tick(60)
     tick = (tick + 1) % 4294967296
     
-    #if tick % 2 == 0 and cursor <= line_length:
-    #    cursor += 1
-    #    label = font.render(line[:cursor], 0, (0xff,0xff,0xff))
-    #    display.blit(label, (10,10))
-    
     dbox.update(tick)
     dbox.render(display)
     
     pygame.display.flip()
     display.fill((0,0,0))
-    
-    #if line.ENDED:
-    #    exit()
-       
+
