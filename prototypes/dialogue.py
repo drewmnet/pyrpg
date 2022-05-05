220c5006d9db33b97b8bc0b92bb531f1cbcdfdb4
Drew M <drewmalette@protonmail.com>
2022-05-04 20:10:45 -0500

    spamming increments

--- prototypes/dialogue.py
+++ prototypes/dialogue.py
@@ -19,13 +19,13 @@ class DialogueBox:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RCTRL:
                     for line in self.lines:
-                        line.cursor = line.text_length
-                        line.update(tick)
-                        self.current = len(self.lines)-1 # so the next line will work                        
+                        line.cursor = line.text_length-1
+                        line.update(tick, self)
+                        self.current = -1 # so the next line will work
                     if self.COMPLETE:
                         self.visible = False
         if not self.lines[-1].ENDED:
-            self.lines[self.current].update(tick)
+            self.lines[self.current].update(tick, self)
             if self.lines[self.current].ENDED:# & self.current < len(self.lines):
                 self.current += 1
         elif not self.COMPLETE and self.lines[-1].ENDED:
@@ -38,32 +38,22 @@ class DialogueBox:
                 surface.blit(line.label, (10, 10 + 24 * i))
 
 class DialogueLine:
-    # do all methods here get called externally (via DialogueBox)?
-    def __init__(self, text, parent): # parent will be DialogueBox
+    def __init__(self, text, parent): # parent=DialogueBox
         self.text = text
         self.text_length = len(self.text)+1
-        self.parent = parent # parent member not required
         
         self.cursor = 0
-        self.label = self.parent.font.render("", 0, (0xff,0xff,0xff))
+        self.label = parent.font.render("", 0, (0xff,0xff,0xff))
         
         # flags
         self.ENDED = False
         
-    def update(self, tick, parent=None): # DialogueBox will pass itself as parent here
-        #print("line update")
+    def update(self, tick, parent): # DialogueBox will pass itself as parent here
         if self.cursor == self.text_length:
-            #if not self.ENDED: print("ENDED")
             self.ENDED = True
         if tick % 2 == 0 and self.cursor <= self.text_length and not self.ENDED:
             self.cursor += 1
-            self.label = self.parent.font.render(self.text[:self.cursor], 0, (0xff,0xff,0xff))
-            #self.label = parent.font.render(self.text[:self.cursor], 0, (0xff,0xff,0xff))
-            
-    def render(self, surface, parent=None):
-        if self.cursor < self.text_length:
-            self.label = self.parent.font.render(self.text[:self.cursor], 0, (0xff,0xff,0xff))
-            surface.blit(self.label, (10,10))
+            self.label = parent.font.render(self.text[:self.cursor], 0, (0xff,0xff,0xff))
 
 pygame.init()
 
